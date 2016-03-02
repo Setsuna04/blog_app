0543218b15536e49a5df2f2f67ded3eeb935ebca
setsuna04 <Setsuna04@gmail.com>
2016-03-02 04:15:18 +0000

    Setup Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+
+root to: "articles#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
