b2fc8f95df82ac023033c7eba2642a55c661b6e5
setsuna04 <Setsuna04@gmail.com>
2016-03-05 23:17:37 +0000

    Create Article successfully

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'articles/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
 
-root to: "articles#index"
+  root to: "articles#index"
+  resources :articles
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
